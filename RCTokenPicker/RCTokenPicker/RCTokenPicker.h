c9730bc8ada375c1bbb3a98eb168dac40cd912f0
Reid Chatham <reid.chatham@gmail.com>
2015-04-19 12:33:28 -0400

    Added x-code framework project

---
+++ RCTokenPicker/RCTokenPicker/RCTokenPicker.h
@@ -0,0 +1,19 @@
+//
+//  RCTokenPicker.h
+//  RCTokenPicker
+//
+//  Created by Reid Chatham on 4/19/15.
+//  Copyright (c) 2015 Chatham Engineering Technologies LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCTokenPicker.
+FOUNDATION_EXPORT double RCTokenPickerVersionNumber;
+
+//! Project version string for RCTokenPicker.
+FOUNDATION_EXPORT const unsigned char RCTokenPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCTokenPicker/PublicHeader.h>
+
+
---
+++ RCTokenPicker/RCTokenPickerTests/RCTokenPickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  RCTokenPickerTests.swift
+//  RCTokenPickerTests
+//
+//  Created by Reid Chatham on 4/19/15.
+//  Copyright (c) 2015 Chatham Engineering Technologies LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RCTokenPickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
